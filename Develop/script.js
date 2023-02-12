3ecffbd6cf37677ec138e1a25382ebc04a1678e8
Andrew Jester <andrewj5787@jesterz.net>
2023-02-12 01:09:47 -0500

    added checkboxes to html, added spacing to checkboxes in css, added checkbox checked check in js, added code to push character choice from checkboxes to array, added random character selection from checked character boxes, added input for how many characters the password should be.

---
+++ Develop/script.js
@@ -0,0 +1,90 @@
+let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let charLow = 'abcdefghijklmnopqrstuvwxyz';
+let numbers = '0123456789';
+let specialChar = '!"#$%&()*+,-./:;<=>?@[]^_`}{|~';
+let userPassLengthInput;
+let upCase = document.getElementById('uppercase');
+let lowCase = document.getElementById('lowercase');
+let numCase = document.getElementById('numbers');
+let specialCase = document.getElementById('special-char');
+let passChar = [];
+let charType
+let char2
+let char3
+
+function random(max) {
+  return Math.floor(Math.random()* max);
+}
+
+function selectPassword() {
+  if(!upCase.checked && !lowCase.checked && !numCase.checked && !specialCase.checked) {
+    alert('You must check at least one checkbox');
+    return;
+  }
+
+  else {
+    passGen();
+  }
+}
+
+function passGen() {
+  if(upCase.checked) {
+    passChar.push(characters);
+  }
+
+  if(lowCase.checked) {
+    passChar.push(charLow);
+  }
+
+  if(numCase.checked) {
+    passChar.push(numbers);
+  }
+
+  if(specialCase.checked) {
+    passChar.push(specialChar);
+  }
+  console.log(passChar);
+  passwordLength();
+}
+
+function passwordLength() {
+  userPassLengthInput = prompt('Enter length of password, has to be a number between 8 and 128.');
+  if (!userPassLengthInput) {
+    passChar = [];
+    return;
+  }
+
+  else {
+    userPassLengthInput = parseInt(userPassLengthInput, 10);
+  }
+
+  if (userPassLengthInput >= 8 && userPassLengthInput <= 128) {
+    for (let i = 0; i < userPassLengthInput; i++) {
+      charType = random(passChar.length);
+      char2 = (random(passChar[charType].length));
+      char3 = passChar[charType][char2];
+      console.log(char3);
+    }
+  }
+
+  else {
+    window.alert('The password length has to be a number between 8 and 128.');
+    passwordLength();
+  }
+  passChar = [];
+}
+
+console.log(characters[6]);
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  selectPassword();
+  // var passwordText = document.querySelector("#password");
+  // passwordText.value = password
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
